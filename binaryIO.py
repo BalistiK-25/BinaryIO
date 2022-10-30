079b06ee2a2978ed114c9e3e0c98cbe4f79dc33a
BalistiK-25 <96784459+BalistiK-25@users.noreply.github.com>
2022-10-30 13:03:52 +0100

    Update binaryIO.py

--- binaryIO.py
+++ binaryIO.py
@@ -1,19 +1,17 @@
-ports = 0
+class MCP:
+    def __init__(self, ports=0):
+        self.ports = ports
 
+    @staticmethod
+    def __port_to_binary(port):
+        return 1 << port
 
-def __port_to_binary(port):
-    return 1 << (port - 1)
+    def get_state(self, port):
+        return (self.ports & MCP.__port_to_binary(port)) >> port
 
+    def set_state(self, state, port):
+        self.ports = self.ports | MCP.__port_to_binary(port) if state == 1 else self.ports & ~MCP.__port_to_binary(port)
 
-def get_state(port):
-    return (ports & __port_to_binary(port)) >> (port - 1)
-
-
-def set_state(state, port):
-    global ports
-    ports = ports | __port_to_binary(port) if state == 1 else ports & ~__port_to_binary(port)
-
-
-def set_state_of_ports(state, port_array):
-    for p in port_array:
-        set_state(state, p)
+    def set_state_of_ports(self, state, port_array):
+        for p in port_array:
+            self.set_state(state, p)
